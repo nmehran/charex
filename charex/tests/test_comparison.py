111ceb62285fcd8e529378ca26a30473d1b69b87
Nima Mehrani <ensigntec@gmail.com>
2022-09-03 02:41:23 -0400

    test_comparison.py::test_equal: More tests

--- charex/tests/test_comparison.py
+++ charex/tests/test_comparison.py
@@ -11,44 +11,6 @@ def test_equal():
     def numba_char_equal(x1, x2):
         return np.char.equal(x1, x2)
 
-    print('\ntest_equal::String Tests:')
-
-    comparison = numba_char_equal(S, S) == np.char.equal(S, S)
-    assert np.all(comparison)
-
-    comparison = numba_char_equal(S, T) == np.char.equal(S, T)
-    assert np.all(comparison)
-
-    comparison = numba_char_equal(T, S) == np.char.equal(T, S)
-    assert np.all(comparison)
-
-    comparison = numba_char_equal(S, 'hello') == np.char.equal(S, 'hello')
-    assert np.all(comparison)
-
-    comparison = numba_char_equal('hello', S) == np.char.equal('hello', S)
-    assert np.all(comparison)
-
-    comparison = numba_char_equal('hello', 'hello') == np.char.equal('hello', 'hello')
-    assert np.all(comparison)
-
-    measure_performance(numba_char_equal, 10, S, S)
-    measure_performance(np.char.equal, 10, S, S)
-
-    measure_performance(numba_char_equal, 10, S, T)
-    measure_performance(np.char.equal, 10, S, T)
-
-    measure_performance(numba_char_equal, 10, T, S)
-    measure_performance(np.char.equal, 10, T, S)
-
-    measure_performance(numba_char_equal, 10, S, 'hello')
-    measure_performance(np.char.equal, 10, S, 'hello')
-
-    measure_performance(numba_char_equal, 10, 'hello', S)
-    measure_performance(np.char.equal, 10, 'hello', S)
-
-    measure_performance(numba_char_equal, 10, 'hello', 'hello')
-    measure_performance(np.char.equal, 10, 'hello', 'hello')
-
     print('\ntest_equal::Byte Tests:')
 
     comparison = numba_char_equal(B, B) == np.char.equal(B, B)
@@ -66,7 +28,13 @@ def test_equal():
     comparison = numba_char_equal(b'hello', B) == np.char.equal(b'hello', B)
     assert np.all(comparison)
 
-    comparison = numba_char_equal(b'hello', b'hello') == np.char.equal(b'hello', b'hello')
+    comparison = numba_char_equal(b'hello', b'hella') == np.char.equal(b'hello', b'hella')
+    assert np.all(comparison)
+
+    comparison = numba_char_equal(b'hello'*100, b'hello'*100) == np.char.equal(b'hello'*100, b'hello'*100)
+    assert np.all(comparison)
+
+    comparison = numba_char_equal(D, E) == np.char.equal(D, E)
     assert np.all(comparison)
 
     measure_performance(numba_char_equal, 10, B, B)
@@ -84,8 +52,64 @@ def test_equal():
     measure_performance(numba_char_equal, 10, b'hello', B)
     measure_performance(np.char.equal, 10, b'hello', B)
 
-    measure_performance(numba_char_equal, 10, b'hello', b'hello')
-    measure_performance(np.char.equal, 10, b'hello', b'hello')
+    measure_performance(numba_char_equal, 10, b'hello', b'hella')
+    measure_performance(np.char.equal, 10, b'hello', b'hella')
+
+    measure_performance(numba_char_equal, 10, b'hello'*1000, b'hello'*1000)
+    measure_performance(np.char.equal, 10, b'hello'*1000, b'hello'*1000)
+
+    measure_performance(numba_char_equal, 10, D, E)
+    measure_performance(np.char.equal, 10, D, E)
+
+    print('\ntest_equal::String Tests:')
+
+    comparison = numba_char_equal(S, S) == np.char.equal(S, S)
+    assert np.all(comparison)
+
+    comparison = numba_char_equal(S, T) == np.char.equal(S, T)
+    assert np.all(comparison)
+
+    comparison = numba_char_equal(T, S) == np.char.equal(T, S)
+    assert np.all(comparison)
+
+    comparison = numba_char_equal(S, 'hello') == np.char.equal(S, 'hello')
+    assert np.all(comparison)
+
+    comparison = numba_char_equal('hello', S) == np.char.equal('hello', S)
+    assert np.all(comparison)
+
+    comparison = numba_char_equal('hello', 'hella') == np.char.equal('hello', 'hella')
+    assert np.all(comparison)
+
+    comparison = numba_char_equal('hello'*100, 'hello'*100) == np.char.equal('hello'*100, 'hello'*100)
+    assert np.all(comparison)
+
+    comparison = numba_char_equal(U, V) == np.char.equal(U, V)
+    assert np.all(comparison)
+
+    measure_performance(numba_char_equal, 10, S, S)
+    measure_performance(np.char.equal, 10, S, S)
+
+    measure_performance(numba_char_equal, 10, S, T)
+    measure_performance(np.char.equal, 10, S, T)
+
+    measure_performance(numba_char_equal, 10, T, S)
+    measure_performance(np.char.equal, 10, T, S)
+
+    measure_performance(numba_char_equal, 10, S, 'hello')
+    measure_performance(np.char.equal, 10, S, 'hello')
+
+    measure_performance(numba_char_equal, 10, 'hello', S)
+    measure_performance(np.char.equal, 10, 'hello', S)
+
+    measure_performance(numba_char_equal, 10, 'hello', 'hella')
+    measure_performance(np.char.equal, 10, 'hello', 'hella')
+
+    measure_performance(numba_char_equal, 10, 'hello'*1000, 'hello'*1000)
+    measure_performance(np.char.equal, 10, 'hello'*1000, 'hello'*1000)
+
+    measure_performance(numba_char_equal, 10, U, V)
+    measure_performance(np.char.equal, 10, U, V)
 
 
 def main():
@@ -95,10 +119,14 @@ def main():
 if __name__ == '__main__':
     np.random.seed(1)
 
-    B = np.random.choice([b'hello', b'all', b'worlds'], 10_000)
+    B = np.random.choice([b'hello', b'all', b'worlds'], 10_000).astype('S200')
     C = np.random.choice([b'hello', b'all', b'worlds'], 10_000).astype('S200')
+    D = np.random.choice([chr(__i) for __i in range(1, 255)], 10_000)
+    E = np.random.choice([chr(__i) for __i in range(1, 255)], 10_000)
 
     S = B.astype('U')
     T = C.astype('U')
+    U = D.astype('U')
+    V = E.astype('U')
 
     main()
