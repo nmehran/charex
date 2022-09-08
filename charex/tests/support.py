9f72e8aa1639552cde6b58a38f085150b7dd0f51
Nima Mehrani <ensigntec@gmail.com>
2022-09-08 16:52:55 -0400

    Refactor test suite

--- charex/tests/support.py
+++ charex/tests/support.py
@@ -1,5 +1,113 @@
 from time import perf_counter, process_time_ns
-from numpy import median, ndarray, zeros
+from numpy import all, array_split, median, ndarray, zeros
+from pandas import DataFrame, Series
+import matplotlib.pyplot as plt
+
+
+class CharacterFunctionTest:
+    def __init__(self, impl, base, byte_args, string_args):
+        self.name_impl = impl.__name__
+        self.name_base = base.__name__
+        self.test_count = 0
+
+        self.impl = impl
+        self.base = base
+        self.byte_args = byte_args
+        self.string_args = string_args
+
+        self.is_measured = False
+        self.is_tested = False
+        self.measurements: list = [[], []]
+        self.plots = None
+
+    def test(self):
+        for args in self.byte_args:
+            run_test(self.impl, self.base, *args)
+        for args in self.string_args:
+            run_test(self.impl, self.base, *args)
+        self.is_tested = True
+
+    def measure(self):
+        if not self.is_tested:
+            self.test()
+        for args in self.byte_args:
+            self.measurements[0].append(median(measure_test(self.impl, self.base, *args), axis=1)*1000)
+        for args in self.string_args:
+            self.measurements[1].append(median(measure_test(self.impl, self.base, *args), axis=1)*1000)
+        self.is_measured = True
+
+    def graph(self):
+        if not self.is_measured:
+            self.measure()
+        self.plots = [
+            graph_performance(measurements=self.measurements[0], func_count=1, test_names=[f'bytes: {self.name_impl}']),
+            graph_performance(measurements=self.measurements[1], func_count=1, test_names=[f'string: {self.name_impl}'])
+        ]
+
+    def run(self, method: str):
+        methods = {'test': self.test, 'measure': self.measure, 'graph': self.graph}
+        run_method = methods.get(method)
+        if not run_method:
+            print("Method must be in ('test', 'measure', 'graph')")
+        else:
+            run_method()
+
+
+def run_test(implementation, baseline, *args, **kwargs) -> None:
+    """Assert implementation reflects the baseline"""
+    if '__msg' in kwargs:
+        print(__msg := kwargs.pop('__msg'))
+    comparison = implementation(*args, **kwargs) == baseline(*args, **kwargs)
+    assert all(comparison)
+
+
+def measure_test(implementation, baseline, *args, **kwargs):
+    """Return pair of measurements for implementation and baseline"""
+    m1 = measure_performance(implementation, 10, *args, **kwargs)
+    m2 = measure_performance(baseline, 10, *args, **kwargs)
+    return m1, m2
+
+
+def test_char_function(func, baseline, byte_arguments, string_arguments):
+    if not hasattr(test_char_function, 'count'):
+        test_char_function.count = 0
+        test_char_function.test_names = []
+        test_char_function.measurements = [[], []]
+
+    test_char_function.count += 1
+    test_char_function.test_names.append(baseline.__name__)
+    test_char_function.measurements[0] += test_and_measure(func, baseline, byte_arguments, __msg='Byte Tests')
+    test_char_function.measurements[1] += test_and_measure(func, baseline, string_arguments, __msg='String Tests')
+
+
+def test_and_measure(impl, base, *args, **kwargs):
+    """
+    Test implementation against baseline and yield a list of tuple measurements
+
+    Parameters
+    ----------
+    impl : function
+        implementation of function which mirrors baseline
+    base: function
+        baseline function which against implementation is tested
+    args : arguments, optional
+        list of function arguments to be used in tests
+    kwargs : key-word arguments, optional
+        function key-word arguments to be used in tests
+    """
+
+    test_label = impl.__name__
+    if '__msg' in kwargs:
+        print(f"\n{test_label!r}::{kwargs.pop('__msg')}:")
+
+    for i, arguments in enumerate(*args, start=1):
+        run_test(impl, base, *arguments, __msg=i)
+        yield (f'{test_label}_{i}',
+               *median(measure_test(impl, base, *arguments), axis=1) * 1000)
+
+    for i, arguments in enumerate(*args, start=1):
+        run_test(impl, base, *arguments[::-1], __msg=i)
+        measure_test(impl, base, *arguments[::-1])
 
 
 def measure_performance(func, n: int = 5, *args, **kwargs) -> ndarray:
@@ -10,6 +118,7 @@ def measure_performance(func, n: int = 5, *args, **kwargs) -> ndarray:
 
     Parameters
     ----------
+    func: function
     n : int : default = 5
         number of iterations to compute median time
     args : arguments, optional
@@ -29,8 +138,55 @@ def measure_performance(func, n: int = 5, *args, **kwargs) -> ndarray:
         end_times[i] = (perf_counter() - start_time)
     _ = type(result)
 
-    if not hasattr(func, '__name__'):
-        func.__name__ = f'f_{process_time_ns()}'
-    print(f'{func.__name__!r}: completed in {median(end_times):.10f} seconds')
+    func_name = f'f_{process_time_ns()}' if not hasattr(func, '__name__') else func.__name__
+    print(f'{func_name!r}: completed in {median(end_times):.10f} seconds')
 
     return end_times
+
+
+def graph_performance(measurements, func_count: int, test_names: list):
+
+    def set_fig_size(test_count, columns, rows, scale_factor=0.6, size_y=7):
+        x_dim = scale_factor * test_count * columns
+        y_dim = rows * size_y
+        return x_dim, y_dim
+
+    def plot_graph(graph, titles, columns=1, main_title=None, as_subplot=False):
+        samples = len(graph)
+        test_count = samples // func_count
+        rows = max(func_count // columns, 1)
+        x_ticks = range(test_count)
+
+        fig = axes = None
+        if func_count == 1:
+            functions = [graph]
+        else:
+            functions = array_split(graph, func_count)
+            fig, axes = plt.subplots(rows, columns)
+
+        fig_size = set_fig_size(test_count, columns, rows)
+        figs = []
+        f = 0
+        for r in range(rows):
+            for c in range(columns):
+                fp = functions[f].plot(ax=axes and axes[r, c],
+                                       figsize=fig_size,
+                                       kind='bar',
+                                       logy=True,
+                                       title=titles[f],
+                                       xlabel='test number',
+                                       xticks=x_ticks,
+                                       ylabel='median time (milliseconds)')
+                figs.append(fp)
+                f += 1
+        plt.suptitle(main_title, fontsize=int(fig_size[0]+8))
+        if not as_subplot:
+            plt.show()
+            return figs
+        return fig
+
+    test_names = Series(test_names)
+    figures = plot_graph(graph=DataFrame(measurements, columns=['implementation', 'baseline']),
+                         titles=test_names,
+                         main_title='Measured Test Performance (milliseconds)')
+    return figures
--- charex/tests/test_comparison.py
+++ charex/tests/test_comparison.py
@@ -1,37 +1,9 @@
 """Test numpy character comparison operators"""
 
-from charex.tests.support import measure_performance
+from charex.tests.support import CharacterFunctionTest
 from numba import njit
 import numpy as np
 import charex
-
-
-def run_test(implementation, baseline, *args, **kwargs):
-    if '__msg' in kwargs:
-        print(__msg := kwargs.pop('__msg'))
-    comparison = implementation(*args, **kwargs) == baseline(*args, **kwargs)
-    assert np.all(comparison)
-
-
-def measure_test(implementation, baseline, *args, **kwargs):
-    measure_performance(implementation, 10, *args, **kwargs)
-    measure_performance(baseline, 10, *args, **kwargs)
-
-
-def test_and_measure(func, baseline, *args, **kwargs):
-    if '__msg' in kwargs:
-        print(f"\n{func.__name__!r}::{kwargs.pop('__msg')}:")
-    for i, arguments in enumerate(*args):
-        run_test(func, baseline, *arguments, __msg=i)
-        measure_test(func, baseline, *arguments)
-    for i, arguments in enumerate(*args):
-        run_test(func, baseline, *arguments[::-1], __msg=i)
-        measure_test(func, baseline, *arguments[::-1])
-
-
-def test_char_function(func, baseline, byte_arguments, string_arguments):
-    test_and_measure(func, baseline, byte_arguments, __msg='Byte Tests')
-    test_and_measure(func, baseline, string_arguments, __msg='String Tests')
 
 
 @njit(nogil=True, cache=True)
@@ -64,53 +36,53 @@ def numba_char_less_equal(x1, x2):
     return np.char.less_equal(x1, x2)
 
 
-@njit(nogil=True, cache=True)
-def compare_chararrays(a1, b1, cmp, rstrip):
-    return np.char.compare_chararrays(a1, b1, cmp, rstrip)
-
-
-def main():
+def main(method='graph'):
     np.random.seed(1)
 
-    B = np.random.choice([b'hello', b'all', b'worlds'], 10_000)
-    C = np.random.choice([b'hello', b'\tFrom', b'all', b'Around', b'\nthe' b' World'], 10_000)
-    D = np.random.choice([b'hello', b'all', b'worlds'], 10_000).astype('S200')
-    E = np.random.choice([chr(__i) for __i in range(1, 128)], 10_000)
-    F = np.random.choice(E, 10_000)
+    length = 10_000
 
-    S = B.astype('U')
-    T = C.astype('U')
-    U = D.astype('U')
-    V = E.astype('U')
-    W = F.astype('U')
+    b = np.random.choice([b'hello', b'all', b'worlds'], length)
+    c = np.random.choice([b'hello', b'\tFrom', b'all', b'Around', b'\nthe' b' World'], length)
+    d = np.random.choice([b'hello', b'all', b'worlds'], length).astype('S200')
+    e = np.random.choice([chr(__i) for __i in range(1, 128)], length)
+    f = np.random.choice(e, length)
+
+    s = b.astype('U')
+    t = c.astype('U')
+    u = d.astype('U')
+    v = e.astype('U')
+    w = f.astype('U')
 
     # With an efficient implementation of whitespace removal, trailing \t\n\r\f\v characters can be supported.
-    X = np.random.choice([''.join([chr(np.random.randint(33, 127)) for _ in range(np.random.randint(1, 50))])
-                          for _ in range(100)], 10_000)
-    Y = np.random.choice(X, 10_000)
-    G, H = X.astype('S'), Y.astype('S')
+    x = np.random.choice([''.join([chr(np.random.randint(33, 127)) for _ in range(np.random.randint(1, 50))])
+                          for _ in range(100)], length)
+    y = np.random.choice(x, 10_000)
+    g, h = x.astype('S'), y.astype('S')
 
     byte_arguments = [
-        (B, B), (B, C), (B, D), (E, F), (G, H),
-        (B, b'hello'), (D, b'hello'), (D, b'hello'*100), (D, b'hello'*10_000),
-        (b'hello', b'hella'), (b'hello', b'aello'),  (b'hello', b'yello'), 
+        (b, b), (b, c), (b, d), (c, d), (e, f), (g, h),
+        (b, b'hello'), (d, b'hello'), (d, b'hello'*100), (d, b'hello'*10_000),
+        (np.array(b'hello', dtype='S200'), np.array(b'ello', dtype='S60')),
+        (b'hello', b'hella'), (b'hello', b'aello'),  (b'hello', b'yello'),
         (b'hello' * 1000, b'hello' * 1000), (b'hello', b'hello'*100), (b'hello', b'jello'*10), (b'hello', b'bello'*10),
-        (np.array(b'hello', dtype='S200'), np.array(b'elo', dtype='S60'))
     ]
     string_arguments = [
-        (S, S), (S, T), (S, U), (V, W), (X, Y),
-        (S, 'hello'), (U, 'hello'), (U, 'hello'*100), (U, 'hello'*10_000),
+        (s, s), (s, t), (s, u), (t, u), (v, w), (x, y),
+        (s, 'hello'), (u, 'hello'), (u, 'hello' * 100), (u, 'hello' * 10_000),
+        (np.array('hello', dtype='U200'), np.array('ello', dtype='U60')),
         ('hello', 'hella'), ('hello', 'aello'),  ('hello', 'yello'),
         ('hello' * 1000, 'hello' * 1000), ('hello', 'hello'*100), ('hello', 'jello'*10), ('hello', 'bello'*10),
-        (np.array('hello', dtype='U200'), np.array('elo', dtype='U60'))
     ]
-    test_char_function(numba_char_equal, np.char.equal, byte_arguments, string_arguments)
-    test_char_function(numba_char_not_equal, np.char.not_equal, byte_arguments, string_arguments)
-    test_char_function(numba_char_greater_equal, np.char.greater_equal, byte_arguments, string_arguments)
-    test_char_function(numba_char_greater, np.char.greater, byte_arguments, string_arguments)
-    test_char_function(numba_char_less, np.char.less, byte_arguments, string_arguments)
-    test_char_function(numba_char_less_equal, np.char.less_equal, byte_arguments, string_arguments)
+
+    CharacterFunctionTest(numba_char_equal, np.char.equal, byte_arguments, string_arguments).run(method)
+    CharacterFunctionTest(numba_char_not_equal, np.char.not_equal, byte_arguments, string_arguments).run(method)
+    CharacterFunctionTest(numba_char_greater_equal, np.char.greater_equal, byte_arguments, string_arguments).run(method)
+    CharacterFunctionTest(numba_char_greater, np.char.greater, byte_arguments, string_arguments).run(method)
+    CharacterFunctionTest(numba_char_less, np.char.less, byte_arguments, string_arguments).run(method)
+    CharacterFunctionTest(numba_char_less_equal, np.char.less_equal, byte_arguments, string_arguments).run(method)
 
 
 if __name__ == '__main__':
-    main()
+    main(method='graph')
+
+    # np.recarray(measurements, [('label', 'U50'), ('byte_timings', 'object'), ('string_timings', 'object')])
