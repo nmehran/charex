035970807311ee9beb353597a33461d80fe7c7fe
Nima Mehrani <ensigntec@gmail.com>
2022-09-16 20:34:22 -0400

    Optimize comparison operator performance

--- charex/numpy/overloads/definitions.py
+++ charex/numpy/overloads/definitions.py
@@ -9,32 +9,17 @@ import numpy as np
 
 
 @register_jitable(**JIT_OPTIONS)
-def _set_comparison(chr_array, len_chr, size_chr, cmp_array, len_cmp, size_cmp, inv):
+def _cast_comparison(len_chr, size_chr, len_cmp, size_cmp):
     if len_cmp > 1 and len_cmp != len_chr:
         msg = 'shape mismatch: objects cannot be broadcast to a single shape.  Mismatch is between arg 0 and arg 1.'
         raise ValueError(msg)
+    cast_margin = size_chr - size_cmp
     if len_cmp == 1:
-        cmp = chr_array.reshape(len_chr, size_chr).copy()
+        size_stride = min(size_chr, size_cmp + (cast_margin < 0))
+        size_cmp = 0
+    else:
         size_stride = min(size_chr, size_cmp)
-        cmp[:, :size_stride] -= cmp_array[:size_stride]
-        if inv:
-            return -cmp.ravel(), size_chr
-        return cmp.ravel(), size_chr
-    if size_chr < size_cmp:
-        cmp = cmp_array.reshape(len_chr, size_cmp).copy()
-        cmp[:, :size_chr] -= chr_array.reshape(len_chr, size_chr)
-        if inv:
-            return cmp.ravel(), size_cmp
-        return -cmp.ravel(), size_cmp
-    if size_chr > size_cmp:
-        cmp = chr_array.reshape(len_chr, size_chr).copy()
-        cmp[:, :size_cmp] -= cmp_array.reshape(len_chr, size_cmp)
-        if inv:
-            return -cmp.ravel(), size_cmp
-        return cmp.ravel(), size_cmp
-    if inv:
-        return cmp_array - chr_array, size_cmp
-    return chr_array - cmp_array, size_cmp
+    return size_cmp, size_stride, cast_margin
 
 
 @register_jitable(**JIT_OPTIONS)
@@ -52,21 +37,17 @@ def equal(chr_array, len_chr, size_chr, cmp_array, len_cmp, size_cmp):
     if len_cmp == 1:
         if size_chr < size_cmp:
             return np.zeros(len_chr, 'bool')
-        elif size_chr > size_cmp:
-            equal_to = np.empty(len_chr, 'bool')
+        equal_to = np.empty(len_chr, 'bool')
+        if size_chr > size_cmp:
             for i in range(len_chr):
                 equal_to[i] = (chr_array[ix + size_cmp] == 0
                                and not _compare_any(cmp_array, chr_array[ix:ix + size_cmp]))
                 ix += size_chr
         else:
-            if len_chr > 1:
-                return (chr_array.reshape(len_chr, size_chr) != cmp_array).sum(axis=1) == 0
-            return ~np.array([_compare_any(chr_array, cmp_array)], 'bool')
+            for i in range(len_chr):
+                equal_to[i] = not _compare_any(cmp_array, chr_array[ix:ix + size_cmp])
+                ix += size_chr
     elif len_chr == len_cmp:
-        if size_chr == size_cmp:
-            if size_chr == 1:
-                return chr_array == cmp_array
-            return (chr_array != cmp_array).reshape(len_chr, size_chr).sum(axis=1) == 0
         iy = 0
         equal_to = np.empty(len_chr, 'bool')
         if size_chr < size_cmp:
@@ -81,6 +62,12 @@ def equal(chr_array, len_chr, size_chr, cmp_array, len_cmp, size_cmp):
                                and not _compare_any(chr_array[ix:ix + size_cmp], cmp_array[iy:iy + size_cmp]))
                 ix += size_chr
                 iy += size_cmp
+        else:
+            if size_chr == 1:
+                return chr_array == cmp_array
+            for i in range(len_chr):
+                equal_to[i] = not _compare_any(chr_array[ix:ix + size_chr], cmp_array[ix:ix + size_chr])
+                ix += size_chr
     else:
         msg = 'shape mismatch: objects cannot be broadcast to a single shape.  Mismatch is between arg 0 and arg 1.'
         raise ValueError(msg)
@@ -94,21 +81,17 @@ def not_equal(chr_array, len_chr, size_chr, cmp_array, len_cmp, size_cmp):
     if len_cmp == 1:
         if size_chr < size_cmp:
             return np.ones(len_chr, 'bool')
-        elif size_chr > size_cmp:
-            not_equal_to = np.empty(len_chr, 'bool')
+        not_equal_to = np.empty(len_chr, 'bool')
+        if size_chr > size_cmp:
             for i in range(len_chr):
                 not_equal_to[i] = (chr_array[ix + size_cmp] != 0
                                    or _compare_any(cmp_array, chr_array[ix:ix + size_cmp]))
                 ix += size_chr
         else:
-            if len_chr > 1:
-                return (chr_array.reshape(len_chr, size_chr) != cmp_array).sum(axis=1) != 0
-            return np.array([_compare_any(chr_array, cmp_array)], 'bool')
+            for i in range(len_chr):
+                not_equal_to[i] = _compare_any(cmp_array, chr_array[ix:ix + size_cmp])
+                ix += size_chr
     elif len_chr == len_cmp:
-        if size_chr == size_cmp:
-            if size_chr == 1:
-                return chr_array != cmp_array
-            return (chr_array != cmp_array).reshape(len_chr, size_chr).sum(axis=1) != 0
         iy = 0
         not_equal_to = np.empty(len_chr, 'bool')
         if size_chr < size_cmp:
@@ -123,6 +106,12 @@ def not_equal(chr_array, len_chr, size_chr, cmp_array, len_cmp, size_cmp):
                                    or _compare_any(chr_array[ix:ix + size_cmp], cmp_array[iy:iy + size_cmp]))
                 ix += size_chr
                 iy += size_cmp
+        else:
+            if size_chr == 1:
+                return chr_array != cmp_array
+            for i in range(len_chr):
+                not_equal_to[i] = _compare_any(chr_array[ix:ix + size_chr], cmp_array[ix:ix + size_chr])
+                ix += size_chr
     else:
         msg = 'shape mismatch: objects cannot be broadcast to a single shape.  Mismatch is between arg 0 and arg 1.'
         raise ValueError(msg)
@@ -132,93 +121,97 @@ def not_equal(chr_array, len_chr, size_chr, cmp_array, len_cmp, size_cmp):
 @register_jitable(**JIT_OPTIONS)
 def greater(chr_array, len_chr, size_chr, cmp_array, len_cmp, size_cmp, inv=False):
     """Native Implementation of np.char.greater"""
-    cmp, size_stride = _set_comparison(chr_array, len_chr, size_chr, cmp_array, len_cmp, size_cmp, inv)
+    size_cmp, size_stride, cast_margin = _cast_comparison(len_chr, size_chr, len_cmp, size_cmp)
     greater_than = np.zeros(len_chr, 'bool')
-    size_stride = max(size_chr, size_stride)
-    stride = 0
+    stride = stride_cmp = 0
     for i in range(len_chr):
         for j in range(size_stride):
-            cmp_ord = cmp[stride + j]
+            cmp_ord = chr_array[stride + j] - cmp_array[stride_cmp + j]
             if cmp_ord == 0:
                 continue
+            cmp_ord = (inv and -cmp_ord) or cmp_ord
             if cmp_ord < 0:
                 break
             greater_than[i] = 1
             break
-        stride += size_stride
-    if len_chr == 1 and size_chr > size_cmp and not greater_than and np.flatnonzero(cmp == 0).size == size_stride:
-        return ~greater_than
+        else:
+            greater_than[i] = cast_margin > 0 and chr_array[stride + size_stride]
+        stride += size_chr
+        stride_cmp += size_cmp
     return greater_than
 
 
 @register_jitable(**JIT_OPTIONS)
 def greater_equal(chr_array, len_chr, size_chr, cmp_array, len_cmp, size_cmp, inv=False):
     """Native Implementation of np.char.greater_equal"""
-    cmp, size_stride = _set_comparison(chr_array, len_chr, size_chr, cmp_array, len_cmp, size_cmp, inv)
-    greater_equal_than = np.ones(len_chr, 'bool')
-    size_stride = max(size_chr, size_stride)
-    stride = 0
+    size_cmp, size_stride, cast_margin = _cast_comparison(len_chr, size_chr, len_cmp, size_cmp)
+    greater_equal_than = np.zeros(len_chr, 'bool')
+    stride = stride_cmp = 0
     for i in range(len_chr):
         for j in range(size_stride):
-            cmp_ord = cmp[stride + j]
+            cmp_ord = chr_array[stride + j] - cmp_array[stride_cmp + j]
+            cmp_ord = (inv and -cmp_ord) or cmp_ord
             if cmp_ord > 0:
+                greater_equal_than[i] = True
                 break
             if cmp_ord < 0:
-                greater_equal_than[i] = 0
                 break
-        stride += size_stride
-    if len_chr == 1 and size_chr < size_cmp and greater_equal_than and np.flatnonzero(cmp == 0).size == size_stride:
-        return ~greater_equal_than
+        else:
+            greater_equal_than[i] = cast_margin >= 0 or not cmp_array[stride_cmp + size_stride]
+        stride += size_chr
+        stride_cmp += size_cmp
     return greater_equal_than
 
 
 @register_jitable(**JIT_OPTIONS)
 def less(chr_array, len_chr, size_chr, cmp_array, len_cmp, size_cmp, inv=False):
     """Native Implementation of np.char.less"""
-    cmp, size_stride = _set_comparison(chr_array, len_chr, size_chr, cmp_array, len_cmp, size_cmp, inv)
+    size_cmp, size_stride, cast_margin = _cast_comparison(len_chr, size_chr, len_cmp, size_cmp)
     less_than = np.zeros(len_chr, 'bool')
-    size_stride = max(size_chr, size_stride)
-    stride = 0
+    stride = stride_cmp = 0
     for i in range(len_chr):
         for j in range(size_stride):
-            cmp_ord = cmp[stride + j]
+            cmp_ord = chr_array[stride + j] - cmp_array[stride_cmp + j]
             if cmp_ord == 0:
                 continue
+            cmp_ord = (inv and -cmp_ord) or cmp_ord
             if cmp_ord > 0:
                 break
             less_than[i] = 1
             break
-        stride += size_stride
-    if len_chr == 1 and size_chr < size_cmp and not less_than and np.flatnonzero(cmp == 0).size == size_stride:
-        return ~less_than
+        else:
+            less_than[i] = cast_margin < 0 and cmp_array[stride_cmp + size_stride]
+        stride += size_chr
+        stride_cmp += size_cmp
     return less_than
 
 
 @register_jitable(**JIT_OPTIONS)
 def less_equal(chr_array, len_chr, size_chr, cmp_array, len_cmp, size_cmp, inv=False):
     """Native Implementation of np.char.less_equal"""
-    cmp, size_stride = _set_comparison(chr_array, len_chr, size_chr, cmp_array, len_cmp, size_cmp, inv)
-    less_equal_than = np.ones(len_chr, 'bool')
-    size_stride = max(size_chr, size_stride)
-    stride = 0
+    size_cmp, size_stride, cast_margin = _cast_comparison(len_chr, size_chr, len_cmp, size_cmp)
+    less_equal_than = np.zeros(len_chr, 'bool')
+    stride = stride_cmp = 0
     for i in range(len_chr):
         for j in range(size_stride):
-            cmp_ord = cmp[stride + j]
+            cmp_ord = chr_array[stride + j] - cmp_array[stride_cmp + j]
+            cmp_ord = (inv and -cmp_ord) or cmp_ord
             if cmp_ord < 0:
+                less_equal_than[i] = True
                 break
             if cmp_ord > 0:
-                less_equal_than[i] = 0
                 break
-        stride += size_stride
-    if len_chr == 1 and size_chr > size_cmp and less_equal_than and np.flatnonzero(cmp == 0).size == size_stride:
-        return ~less_equal_than
+        else:
+            less_equal_than[i] = cast_margin <= 0 or not chr_array[stride + size_stride]
+        stride += size_chr
+        stride_cmp += size_cmp
     return less_equal_than
 
 
 @register_jitable(**JIT_OPTIONS)
 def compare_chararrays(chr_array, len_chr, size_chr, cmp_array, len_cmp, size_cmp, inv, cmp):
     """Native Implementation of np.char.compare_chararrays"""
-    # The argument cmp can be passed as bytes or string, hence the ordinal map.
+    # The argument cmp can be passed as bytes or string, hence ordinal mapping.
     # {“<”, “<=”, “==”, “>=”, “>”, “!=”} | { (60,) (60, 61), (61, 61), (62, 61), (62,), (33, 61) }
     if len(cmp) == 1:
         cmp_ord = ord(cmp)
